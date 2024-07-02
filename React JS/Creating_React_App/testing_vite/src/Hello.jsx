ca664f4f1d8ce2a3ff27c4e1c8f04cd0fde6c404
Aman Bijlwan <amanbijlwan2021009@gmail.com>
2024-07-02 23:04:43 +0530

    Some concepts on react app creation added

---
+++ React JS/Creating_React_App/testing_vite/src/Hello.jsx
@@ -0,0 +1,11 @@
+// In vite it is necessary to use " .jsx " extension, wherever HTML content is used in js file
+
+function Bye() {
+    
+
+    return (
+        <h2>Hello </h2>
+    )
+};
+
+export default Bye
