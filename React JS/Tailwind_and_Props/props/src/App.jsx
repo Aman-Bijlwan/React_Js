7dae7700c0ee72fa3e9929635631b5f1464e428f
Aman Bijlwan <bijlwan009aman@gmail.com>
2024-07-10 00:01:28 +0530

    Some concepts of Tailwind CSS and Props

---
+++ React JS/Tailwind_and_Props/props/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from './Card'
+function App() {
+
+  return (
+    <>
+      <Card username="Card 1" btnValue="Click Me"/>
+      <Card username="Card 2" btnValue="Visit Me"/>
+    </>
+  )
+}
+
+export default App
